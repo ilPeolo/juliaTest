01e9fa12d0b01d78eb0b11750ab9443bc36f6c99
ilPeolo <paolo.angella98@gmail.com>
2021-11-20 22:07:23 +0100

    Sta prendendo forma, mannaggia a "collect"

--- sphere.jl
+++ sphere.jl
@@ -1,33 +1,59 @@
 #Vediamo se Diomadonna funziona sta merda
 
 using PyPlot 
+using LinearAlgebra
 
 mutable struct mysurface
     u::Vector{Float64}
     v::Vector{Float64}
-    x::Array{Float64}
-    y::Array{Float64}
-    z::Array{Float64}
+    x::Matrix{Float64}
+    y::Matrix{Float64}
+    z::Matrix{Float64}
+    fx::Function
+    fy::Function
+    fz::Function
 end
-function mysurface(a::Vector{Float64},b::Vector{Float64},fx::function(),fy::function(),fz::function())
-    mysurface(a,b,fx(a,b),fy(a,b),fz(a,b))
+function mysurface(a::Vector{Float64},b::Vector{Float64},fx::Function,fy::Function,fz::Function)
+    mysurface(a,b,fx(a,b),fy(a,b),fz(a,b),fx,fy,fz)
 end
-
-fx(u,v) = cos.(u) * sin.(v)';
-fy(u,v) = sin.(u) * sin.(v)';
-fz(u,v) = ones(n) * cos.(v)';
+function graph(self::mysurface)
+    surf(self.x,self.y,self.z, rstride=4, cstride=4)
+    display(gcf())
+end
 
 n=300;
-u = range(0,stop=2*π,length=n);
-v = range(0,stop=π,length=n);
 
-mysurface(u,v,fx,fy,fz);
+u = collect(range(0,stop=2*π,length=n));
+v = collect(range(0,stop=π,length=n));
+
+function defx(u::Vector{Float64},v::Vector{Float64})::Matrix{Float64}
+    cos.(u) * sin.(v)'
+end
+function defy(u::Vector{Float64},v::Vector{Float64})::Matrix{Float64}
+    sin.(u) * sin.(v)'
+end
+function defz(u::Vector{Float64},v::Vector{Float64})::Matrix{Float64}
+    ones(length(v)) * cos.(v)'
+end
 
 
+sfera= mysurface(u,v,defx,defy,defz);
+#graph(sfera)
 
+mutable struct funzSuSup
+    S :: mysurface
+    funz::Function
+    val::Matrix{Float64}
+end
+function funzSuSup(Spass::mysurface,f::Function)
+    S=Spass
+    funz=f
+    val=f(S.u,S.v)
+end
 
-# The rstride and cstride arguments default to 10
-#surf(x,y,z, rstride=4, cstride=4)
-#display(gcf())
+function funztest(u::Vector{Float64},v::Vector{Float64}):Array{Float64}
+    return map(a -> maximum([a,0]), u*v')
+end
 
-return(0)
+nuovfunz=funzSuSup(sfera,funztest)
+
