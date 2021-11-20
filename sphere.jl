4176e379595d7d07bf8ea615b5d3603b7e0318b3
ilPeolo <paolo.angella98@gmail.com>
2021-11-20 23:05:07 +0100

    FUNZIONAAA

--- sphere.jl
+++ sphere.jl
@@ -17,6 +17,7 @@ function mysurface(a::Vector{Float64},b::Vector{Float64},fx::Function,fy::Functi
     mysurface(a,b,fx(a,b),fy(a,b),fz(a,b),fx,fy,fz)
 end
 function graph(self::mysurface)
+    clf()
     surf(self.x,self.y,self.z, rstride=4, cstride=4)
     display(gcf())
 end
@@ -48,21 +49,43 @@ end
 function funzSuSup(S::mysurface,f::Function)
     funzSuSup(S,f,f(S.u,S.v))
 end
-function funztest(u::Vector{Float64},v::Vector{Float64})
-    return u*v'
+function funztest1(u::Vector{Float64},v::Vector{Float64})
+    a=zeros(n,n)
+    for i = 1:n
+        for j = 1:n
+            if i < 150
+                a[i,j]=1
+            end
+        end
+    end
+    return a
+end
+function funztest2(u::Vector{Float64},v::Vector{Float64})
+    a=zeros(n,n)
+    for i = 1:n
+        for j = 1:n
+            if i > 150
+                a[i,j]=1
+            end
+        end
+    end
+    return a
 end
 function graph(self::funzSuSup)
     norm=0.5 * self.val ./ (maximum(abs.(self.val)))
-    newx=self.S.x .* (norm .k+1)
-    newy=self.S.y .* (norm .+1)
-    newz=self.S.z .* (norm .+1)
+    newx=self.S.x .+ (self.val .*norm)
+    newy=self.S.y .+ (self.val .*norm)
+    newz=self.S.z .+ (self.val .*norm)
+    clf()
     surf(newx,newy,newz, rstride=4, cstride=4)
+    
     display(gcf())
 end
 
-nuovfunz=funzSuSup(sfera,funztest)
+nuovfunz1=funzSuSup(sfera,funztest1)
+nuovfunz2=funzSuSup(sfera,funztest2)
 
-graph(nuovfunz)
+graph(nuovfunz2)
+graph(nuovfunz1)
 
 return(0)
-
