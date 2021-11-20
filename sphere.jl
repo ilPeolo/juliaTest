8933309229afbfca2e6e8a098fbdaee112429c10
ilPeolo <paolo.angella98@gmail.com>
2021-11-21 00:42:16 +0100

    Aggiunto un toro

--- sphere.jl
+++ sphere.jl
@@ -85,7 +85,30 @@ end
 nuovfunz1=funzSuSup(sfera,funztest1)
 nuovfunz2=funzSuSup(sfera,funztest2)
 
-graph(nuovfunz2)
-graph(nuovfunz1)
+#graph(nuovfunz2)
+#graph(nuovfunz1)
 
 return(0)
+
+##toro
+
+u = collect(range(0,stop=2*π,length=n));
+v = collect(range(0,stop=2*π,length=n));
+R=1.3
+r=1
+
+function defx(u::Vector{Float64},v::Vector{Float64})::Matrix{Float64}
+    (R.+(r.*cos.(u)))*cos.(v)'
+end
+function defy(u::Vector{Float64},v::Vector{Float64})::Matrix{Float64}
+    (R.+(r.*cos.(u)))*sin.(v)'
+end
+function defz(u::Vector{Float64},v::Vector{Float64})::Matrix{Float64}
+    ones(length(v)) * r.*sin.(u)'
+end
+
+toro= mysurface(u,v,defx,defy,defz);
+
+graph(toro)
+
+return 0
