8520f8ad54e20e915b53ccc9db42be04f1272324
ilPeolo <paolo.angella98@gmail.com>
2021-11-20 20:01:32 +0100

    Proviamo a definire una struc

--- sphere.jl
+++ sphere.jl
@@ -2,7 +2,16 @@
 
 using PyPlot 
 
-n = 100
+mutable struct mysurface
+    u::Vector{Float64}
+    v::Vector{Float64}
+    x::Array{Float64}
+    y::Array{Float64}
+    z::Array{Float64}
+end
+function mysurface(a::Vector{Float64},b::Vector{Float64},fx,fy,fz)
+    mysurface(a,b,fx(a,b),fy(a,b),fz(a,b))
+end
 u = range(0,stop=2*π,length=n);
 v = range(0,stop=π,length=n);
 
@@ -10,6 +19,7 @@ x = cos.(u) * sin.(v)';
 y = sin.(u) * sin.(v)';
 z = ones(n) * cos.(v)';
 
+
 # The rstride and cstride arguments default to 10
-surf(x,y,z, rstride=4, cstride=4)
-display(gcf())
+#surf(x,y,z, rstride=4, cstride=4)
+#display(gcf())
